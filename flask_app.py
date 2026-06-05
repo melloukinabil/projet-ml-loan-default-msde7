dd05d353ac6469084af28f5135c8a4586b078e19
melloukinabil <nabil.mellouki92@gmail.com>
2026-06-05 07:06:32 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 import pickle
+import os
+os.environ['PYTHONIOENCODING'] = 'utf-8'
 
 app = Flask(__name__)
 
